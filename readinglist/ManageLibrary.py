5a79e1c4aeda35646c373a89a7807234af6abcf0
snshne <davearock+github@gmail.com>
2020-01-19 07:14:44 -0800

    reorganize functions

--- readinglist/ManageLibrary.py
+++ readinglist/ManageLibrary.py
@@ -1,45 +1,5 @@
-def MakeBookCard():
-    '''
-    Make empty questionnaire for user to enter  book information
-    :return:
-    '''
-    book_card = {
-        'title': {'question': 'What is the title of the book?',
-                  'answer': ''},
-        'format': {'question': 'What format was the book read in?',
-                   'answer': '',
-                   'values': ['paperback', 'hardcover', 'audiobook', 'kindle''']
-                   },
-        'fiction': {'question': 'Was the book fiction?',
-                    'answer': '',
-                    'values': [0, 1]
-                    },
-        'completed': {'question': 'Has the book been completed?',
-                      'answer': '',
-                      'values': [0, 1]
-                      },
-        'reread': {'question': 'Was this the first reading?',
-                  'answer': '',
-                   'values': [0, 1]
-                   },
-        'female': {'question': 'Is the author female?',
-                   'answer': '',
-                   'values': [0, 1]
-                   },
-        'poc': {'question': 'Is the author a POC?',
-                'answer': '',
-                'values': [0, 1]
-                },
-        'foreign': {'question': 'Is the author non-American?',
-                    'answer': '',
-                    'values': [0, 1]
-                    },
-        'goodreads': {'question': 'What is the Goodreads book score?',
-                      'answer': '',
-                      'type': float
-                      },
-    }
-    return(book_card)
+from readinglist.CreateLibrary import StartBookCase
+from readinglist.CreateLibrary import MakeBookCard
 
 def GetBookInformation():
     '''
@@ -55,20 +15,6 @@ def GetBookInformation():
         book_information[k]['answer'] = this_answer
 
     return(book_information)
-
-def StartBookCase():
-    book_case = {
-        'title': [],
-        'format': [],
-        'fiction': [],
-        'completed': [],
-        'reread': [],
-        'female': [],
-        'poc': [],
-        'foreign': [],
-        'goodreads': []
-    }
-    return(book_case)
 
 def StoreBook(book_details, book_case = None):
     '''
