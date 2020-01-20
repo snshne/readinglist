20f498562ac0a16bba3f3f1720eb4fa8a6d471e8
snshne <davearock+github@gmail.com>
2020-01-19 17:04:28 -0800

    initialize loading and saving dictionary functions

--- readinglist/ManageLibrary.py
+++ readinglist/ManageLibrary.py
@@ -1,3 +1,6 @@
+import json
+
+
 def store_book(book, book_case=None):
     '''
     This function will put a book onto the book case
@@ -13,3 +16,22 @@ def store_book(book, book_case=None):
         book_case[k] += book[k]
 
     return (book_case)
+
+def open_library(library_path):
+    '''
+    this function will open the library path (json) and load in the entered books
+    :param library_path: path to where the current library dictionary is stored
+    :return: dictionary of library
+    '''
+
+    library = json.load(library_path)
+    return library
+
+def close_libary(library_path):
+    '''
+    this function will save the library dictionary to the library path
+    :param library_path: path to where the current library dictionary is stored
+    '''
+
+    json.dump(library_path)
+
